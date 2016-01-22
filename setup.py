604bfaef265b71a26e4e830263a4bd096713a550
selurvedu <selurvedu@users.noreply.github.com>
2016-01-22 21:12:59 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+name = 'robot_tests.broker.etender'
+
+setup(name=name,
+      version='0.0.dev1',
+      description='E-tender broker for OpenProcurement Robot tests',
+      author='',
+      author_email='',
+      url='https://github.com/openprocurement/{}'.format(name),
+      )

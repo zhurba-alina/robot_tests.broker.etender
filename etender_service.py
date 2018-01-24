cb59ab7b6636bdc1f6446776b2f04b2ce5534626
Victor Utechenko <Victor.Utechenko@softengi.com>
2018-01-24 14:05:18 +0200

    Update date and string converter helpers

--- etender_service.py
+++ etender_service.py
@@ -1,20 +1,15 @@
 # -*- coding: utf-8 -
 
 from iso8601 import parse_date
+from datetime import datetime, date, time, timedelta
 import dateutil.parser
 
 
 def get_all_etender_dates(initial_tender_data, key, subkey=None):
     tender_period = initial_tender_data.data.tenderPeriod
-    enquiry_period = initial_tender_data.data.enquiryPeriod
-    end_period = dateutil.parser.parse(enquiry_period['endDate'])
     start_dt = dateutil.parser.parse(tender_period['startDate'])
     end_dt = dateutil.parser.parse(tender_period['endDate'])
     data = {
-        'EndPeriod': {
-            'date': end_period.strftime("%d-%m-%Y"),
-            'time': end_period.strftime("%H:%M"),
-        },
         'StartDate': {
             'date': start_dt.strftime("%d-%m-%Y"),
             'time': start_dt.strftime("%H:%M"),
@@ -24,6 +19,13 @@ def get_all_etender_dates(initial_tender_data, key, subkey=None):
             'time': end_dt.strftime("%H:%M"),
         },
     }
+    if 'enquiryPeriod' in initial_tender_data.data:
+        enquiry_period = initial_tender_data.data.enquiryPeriod
+        end_period = dateutil.parser.parse(enquiry_period['endDate'])
+        data['EndPeriod'] = {
+            'date': end_period.strftime("%d-%m-%Y"),
+            'time': end_period.strftime("%H:%M"),
+        }
     dt = data.get(key, {})
     return dt.get(subkey) if subkey else dt
 
@@ -46,25 +48,48 @@ def convert_time_to_etender_format(isodate):
     return time_string
 
 
+def float_to_string_2f(value):
+    return '{:.2f}'.format(value)
+
+
 def string_to_float(string):
     return float(string)
 
 
 def change_data(initial_data):
+    #TODO: remove redundant hardcoded values
     initial_data['data']['items'][0]['deliveryAddress']['locality'] = u"м. Київ"
     initial_data['data']['items'][0]['deliveryAddress']['region'] = u"Київська область"
-    initial_data['data']['items'][0]['classification']['description'] = u"Картонні коробки"
-    initial_data['data']['procuringEntity']['name'] = u"Степанов-Зайцева"
-    initial_data['data']['minimalStep']['amount'] = 750.11
+    initial_data['data']['items'][0]['classification']['description'] = u"Паперові чи картонні реєстраційні журнали, бухгалтерські книги, швидкозшивачі, бланки та інші паперові канцелярські вироби"
+    initial_data['data']['items'][0]['unit']['name'] = u"кілограми"
+    initial_data['data']['procuringEntity']['name'] = u"TenderOwner#"
     return initial_data
 
 
+def convert_etender_date_to_iso_format(date_time_from_ui):
+    new_timedata = datetime.strptime(date_time_from_ui, '%d-%m-%Y, %H:%M')
+    new_date_time_string = new_timedata.strftime("%Y-%m-%d %H:%M:%S.%f")
+    return new_date_time_string
+
+
+def convert_common_string_to_etender_string(string):
+    dict = get_helper_dictionary()
+    for key, val in dict.iteritems():
+        if val == string:
+            return key
+    return string
+
+
 def convert_etender_string_to_common_string(string):
+    dict = get_helper_dictionary()
+    return dict.get(string, string)
+
+
+def get_helper_dictionary():
     return {
         u"Київська область": u"м. Київ",
         u"Київ": u"м. Київ",
         u"кг.": u"кілограм",
         u"грн.": u"UAH",
         u"(включаючи ПДВ)": True,
-        500.01: 100.1,
-    }.get(string, string)
+    }
